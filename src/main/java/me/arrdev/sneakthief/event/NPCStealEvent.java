b2964eac84cc13478cdd0fd6237537d92e4f2f9c
darkman2412 <rafvdl@hotmail.com>
2013-05-01 22:01:01 +0200

    PlayerInventory -> Inventory.

--- src/main/java/me/arrdev/sneakthief/event/NPCStealEvent.java
+++ src/main/java/me/arrdev/sneakthief/event/NPCStealEvent.java
@@ -2,20 +2,20 @@ package me.arrdev.sneakthief.event;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.Inventory;
 
 public class NPCStealEvent extends PlayerStealEvent {
 
-	private final PlayerInventory npcInv;
+	private final Inventory npcInv;
 
 	private static final HandlerList handlers = new HandlerList();
 
-	public NPCStealEvent(Player who, Player pp, PlayerInventory inv) {
+	public NPCStealEvent(Player who, Player pp, Inventory inv) {
 		super(who, pp);
 		this.npcInv = inv;
 	}
 
-	public PlayerInventory getInventory() {
+	public Inventory getInventory() {
 		return npcInv;
 	}
 
